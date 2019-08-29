db3d4e126e693374162e06642f130f81c32be83e
Matthew Nielsen <mnielsen10521@gmail.com>
2019-08-29 11:19:04 +1000

    practicle

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -6,7 +6,7 @@ def main():
 MINIMUM_LENGTH = 5
 
 
-def version_1():
+def original():
     password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
     while len(password) < MINIMUM_LENGTH:
         password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
